286d8201c4ee6d843e27e226d1d06334de2e5ec4
mathewpeedikayil <mathewphilip.peedikayil@gmail.com>
2023-06-23 12:55:29 +0100

    Created a random number between the range 0 and 100

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -1,3 +1,9 @@
+import java.util.Random;
+
 public class GuessTheNumber {
-    
+
+    Random rand = new Random();
+    private static final int NUMBER_RANGE = 101;
+    int randomNumber = rand.nextInt(NUMBER_RANGE); // range between 0 and 100
+
 }
