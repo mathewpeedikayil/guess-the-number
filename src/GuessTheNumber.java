2b80a086a51164a1dd3a9bd682628632132f5c6d
mathewpeedikayil <mathewphilip.peedikayil@gmail.com>
2023-06-23 13:40:41 +0100

    Added cases if the number guessed is higher than the random number and lower than the random number

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -10,10 +10,14 @@ public class GuessTheNumber {
     private static final String WELCOME_MSG = "--- Welcome to Guess The Number ! ---";
     private static final String QUIT_MSG = "--- Type 'quit' to end game at anytime ---";
     private static final String GUESS_MSG = "Guess a number between 0 and 100: ";
+    private static final String WIN_MSG = "Brilliant! You have guessed correctly!";
+    private static final String HIGH_NUMBER_MSG = "Guess again! You have guessed a higher number!";
+    private static final String LOW_NUMBER_MSG = "Guess again! You have guessed a lower number!";
     private static final String THANKS_PLAYING_MSG = "Thanks for playing !";
 
     private int randomNumber = rand.nextInt(NUMBER_RANGE); // range between 0 and 100
     private String userInput;
+    private int guessedNumber;
     private boolean isEnded;
 
     /* initiates the game. */
@@ -28,6 +32,15 @@ public class GuessTheNumber {
             userInput = scn.nextLine();
             if(userInput.equals("quit")) {
                 isEnded = true;
+            } else {
+                guessedNumber = Integer.parseInt(userInput);
+                if(guessedNumber == randomNumber) {
+                    System.out.println(WIN_MSG);
+                } else if(randomNumber > guessedNumber) {
+                    System.out.println(HIGH_NUMBER_MSG);
+                } else {
+                    System.out.println(LOW_NUMBER_MSG);
+                }
             }
         }
         scn.close();
